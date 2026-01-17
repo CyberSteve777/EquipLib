b722fcc4464980e4721c9c51b05b9df834cac1ec
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2026-01-17 22:26:19 +0300

    Revert "backport from 1.20.1 without codecs"
    This reverts commit 241c19d0ce83b83cf755c2c5a9bb0b71e4f6d71b.

--- common/src/main/java/me/cybersteve/equiplib/util/EffectMeta.java
+++
@@ -1,7 +0,0 @@
-package me.cybersteve.equiplib.util;
-
-/**
- * Effect meta is like MobEffectInstance.Details, but a bit cut in functionality and with custom methods for creation
- */
-public record EffectMeta(int duration, int amplifier, boolean ambient, boolean showParticles, boolean showIcon) {
-}
